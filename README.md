f4ddb72496278611473e81d413eb489384e4c5f8
Nogmanov Ildar <nogmanov@gmail.com>
2017-03-19 06:42:36 +0300

    Friday work2

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Example of script launch on Linux, Python 3.5:
 
 ```#!bash
 
-$ python pprint_json.py <path to file>
+$ python pprint_json.py python3 pprint_json.py /home/ildar/Projects/DevMan/4_json/models.json
 # TODO add output example
 
 ```
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,30 @@
+''' TODO
+[x] 1 doc -> README.md
+[x] 2 path
+[x] 3 comments, why? What's this?
+[ ] readme
+[ ] cyrillic
+'''
+
+from sys import argv
 import json
+import os
+
+# models.json in repo
+# filepath = '/home/ildar/Projects/DevMan/4_json/models.json'
+filepath = argv[1]
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+    	return None
+    with open(filepath, 'r') as file_handler:
+    	return json.load(file_handler) 			# ? OR return data = json.load...
 
 
 def pretty_print_json(data):
-    pass
+    return print(json.dumps(data, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    pretty_print_json(load_data(filepath))
