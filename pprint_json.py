1f535cd5ed4424b872b5ad01017d260bae062f1b
Nogmanov Ildar <nogmanov@gmail.com>
2017-04-21 19:30:34 +0300

    Remove json file

--- pprint_json.py
+++ pprint_json.py
@@ -1,25 +1,14 @@
-''' TODO
-[x] 1 doc -> README.md
-[x] 2 path
-[x] 3 comments, why? What's this?
-[ ] readme
-[ ] cyrillic
-'''
-
 from sys import argv
 import json
 import os
 
-# models.json in repo
-# filepath = '/home/ildar/Projects/DevMan/4_json/models.json'
-filepath = argv[1]
-
 
 def load_data(filepath):
+    filepath = argv[1]
     if not os.path.exists(filepath):
     	return None
     with open(filepath, 'r') as file_handler:
-    	return json.load(file_handler) 			# ? OR return data = json.load...
+    	return json.load(file_handler) 
 
 
 def pretty_print_json(data):
